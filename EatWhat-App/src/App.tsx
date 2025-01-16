ce5c723d04b1af77a1c3c52b770b0ca1d9e08b22
caslawter <caslawter@gmail.com>
2025-01-17 00:51:42 +0800

    Added google api to frontend

--- EatWhat-App/src/App.tsx
+++ EatWhat-App/src/App.tsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { useWebApp } from "@vkruglikov/react-telegram-web-app";
 import { useThemeParams } from '@vkruglikov/react-telegram-web-app';
+import axios from "axios";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -17,6 +18,27 @@ function App() {
     button_color: themeParams.button_color,
     bg_color: themeParams.bg_color,
   });
+
+  useEffect(() => {
+    async function getGoogleResults() {
+      try {
+        let results = await axios.get(`http://localhost:8000/api/places`,{
+          params: {
+            lat: 1.2976174485362484,
+            lng:103.85488811330647,
+            radius:5000,
+            min:1,
+            max:1
+          },
+        });
+        console.log(results);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getGoogleResults();
+  }, []);
+
 
   return (
     <>
