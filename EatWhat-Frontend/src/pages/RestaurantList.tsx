6ed06c203242a3d5dfd1e53b1d8b65b21084a337
wei bin <lauweibin77@gmail.com>
2025-01-18 18:16:05 +0800

    not really much

--- EatWhat-Frontend/src/pages/RestaurantList.tsx
+++ EatWhat-Frontend/src/pages/RestaurantList.tsx
@@ -6,60 +6,66 @@ import DialogueBox from "../components/DialogueBox";
 import { FaDollarSign } from "react-icons/fa";
 import { FaStar } from "react-icons/fa6";
 import { BackButton, useWebApp } from "@vkruglikov/react-telegram-web-app";
+import type { ModalOptions, ModalInterface } from "flowbite";
+import { input } from "flowbite";
 
 interface RestaurantObject {
   name: string;
   location: string;
   rating: number;
   priceLevel: number;
-  type: string;
+  type: string | null;
 }
 
-const RestaurantCard = (): JSX.Element => {
-  const [checked, setChecked] = useState<boolean>(false);
-  const restaurant_1: RestaurantObject = {
-    name: "muthu curry",
-    location: "82 Orchard Rd, Singapore 238839",
-    rating: 3.7,
-    priceLevel: 1,
-    type: "Indian",
-  };
-  return (
-    <div className="bordrer-b-2 flex items-center w-full justify-between">
-      {/* left and right */}
-      <div className="flex flex-col gap-2 w-10/12">
-        {/* top and down */}
-        <div className="relative flex item-center">
-          {/* top */}
-          <h3 className="font-bold text-lg">{restaurant_1.name}</h3>
+// const RestaurantCard = ({restaurant}): JSX.Element => {
+//   const [checked, setChecked] = useState<boolean>(false);
+//   // const restaurant_1: RestaurantObject = {
+//   //   name: "muthu curry",
+//   //   location: "82 Orchard Rd, Singapore 238839",
+//   //   rating: 3.7,
+//   //   priceLevel: 1,
+//   //   type: "Indian",
+//   // };
+//   return (
+//     <div className="border-b pb-2 border-white flex items-center w-full justify-between align-middle">
+//       {/* left and right */}
+//       <div className="flex flex-col gap-2 w-10/12">
+//         {/* top and down */}
+//         <div className="relative flex item-center text-center ">
+//           {/* top */}
+//           <h3 className="font-bold text-lg ">{restaurant.name}</h3>
 
-          <div className="flex items-center absolute right-0">
-            <FaStar className="text-customOrange-dark" />
-            <p>{restaurant_1.rating}</p>
-          </div>
-        </div>
-        <p>{restaurant_1.type}</p>
-        <div className="flex items-center">
-          <FaDollarSign className="text-customOrange-dark" />
-          <FaDollarSign className="text-gray-300" />
-          <FaDollarSign className="text-gray-300" />
-          <FaDollarSign className="text-gray-300" />
-        </div>
-        {/* Down */}
-        <p>{restaurant_1.location}</p>
-      </div>
-      </div>
-      )
-}
+//           <div className="flex items-center absolute right-0 h-full">
+//             <FaStar className="text-customOrange-dark" />
+//             <p>{restaurant.rating}</p>
+//           </div>
+//         </div>
+//         <p>{restaurant.type}</p>
+//         <div className="flex items-center">
+//           <FaDollarSign className="text-customOrange-dark" />
+//           <FaDollarSign className="text-gray-300" />
+//           <FaDollarSign className="text-gray-300" />
+//           <FaDollarSign className="text-gray-300" />
+//         </div>
+//         {/* Down */}
+//         <p>{restaurant.location}</p>
+//       </div>
+
+//       <input type="checkbox" id="custom-checkbox" className="" />
+//     </div>
+//   );
+// };
+
+const cuisineTypes: string[] = ["chinese", "malay", "indian", "mexican"];
 
 const RestaurantList = (): JSX.Element => {
-  // const [restaurantDetail,setRestaurantList]=useState<null | RestaurantObject >(null);
+  const [restaurantList, setRestaurantList] = useState<RestaurantObject[]>([]);
   const navigate = useNavigate();
   useEffect(() => {
     async function getGoogleResults() {
       try {
         const results = await axios.get(
-          `${import.meta.env.VITE_BACKEND_PORT}/api/places`,
+          `${import.meta.env.VITE_BACKEND_PORT}/api/searchArea`,
           {
             params: {
               lat: 1.2976174485362484,
@@ -71,6 +77,30 @@ const RestaurantList = (): JSX.Element => {
           }
         );
         console.log(results);
+        results.data.map((restaurant) => {
+          // FOR EACH Restaurant,
+          // query second api, put it in a list for types
+          let placeDetailsResponse: object;
+          axios
+            .get(`${import.meta.env.VITE_BACKEND_PORT}/api/placeDetails`, {
+              params: {
+                placeID: restaurant.place_id,
+              },
+            })
+            .then((response) => {
+              placeDetailsResponse = response.data;
+              console.log("placeDetailsResponse ==> ", placeDetailsResponse);
+            });
+          // run a function that filters for cuisine type based on regex
+          // store the variable inside
+          // create the object and put inside the results
+          const restaurantObj: RestaurantObject = {
+            name: restaurant.name,
+            location: restaurant.vicinity,
+            rating: restaurant.rating,
+            priceLevel: restaurant.price_level,
+          };
+        });
       } catch (error) {
         console.log(error);
       }
@@ -79,36 +109,23 @@ const RestaurantList = (): JSX.Element => {
   }, []);
 
   return (
-    <>
-      <BackButton onClick={() => navigate(-1)}/>
-      <div className="">
-        {/* elements at the top */}
-        <div className="flex mb-10">
-          <Link
-            to="/preference"
-            className="font-bold flex gap-2 text-3xl text-customOrange-dark hover:text-customOrange-light hover:underline"
-          >
-            <p>&lt;</p>
-            <h2>Preferences</h2>
-            {/* TODO: add thee right icons */}
-          </Link>
-        </div>
+    <div>
+      <BackButton onClick={() => navigate(-1)} />
 
-      <div>
+      {/* <div className="flex flex-col gap-5 mt-2">
         <RestaurantCard />
         <RestaurantCard />
         <RestaurantCard />
-        <div>
-          <RestaurantCard />
-          <RestaurantCard />
-          <RestaurantCard />
-        </div>
-      </div>
+      </div> */}
+
+      <Link
+        to="/Map"
+        className=" absolute bottom-1 mt-5 text-orange-400 hover:bg-customOrange-dark hover:text-white  border border-orange-400 p-2 rounded-3xl text-xl w-11/12 flex  justify-center"
+      >
+        <button>Next</button>
+      </Link>
     </div>
-    </>
   );
 };
-
-
 
 export default RestaurantList;
--- EatWhat-Frontend/tailwind.config.js
+++ EatWhat-Frontend/tailwind.config.js
@@ -1,55 +1,38 @@
 /** @type {import('tailwindcss').Config} */
-import flowbitePlugin from 'flowbite/plugin';
 export default {
   mode: 'jit',
-
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}","./node_modules/flowbite/**/*.js"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
       fontFamily: {
         'open-sans': ['"Open Sans"', 'serif'],
         bagel: ['"Bagel Fat One"', 'cursive'],
       },
-      fontWeight: {
-        300: '300',
-        400: '400',
-        600: '600',
-        700: '700',
-        800: '800',
-      },
       screens: {
-        'xs': '320px',   // Extra small screens
-        'xss': '375px',
-        'xsm': '425px',  // Extra small medium screens
-        'sm': '640px',   // Small screens
-        'md': '768px',   // Medium screens
-        'lg': '1024px',  // Large screens
-        'xl': '1280px',  // Extra large screens
+        xs: '320px', // Extra small screens
+        xss: '375px',
+        xsm: '425px', // Extra small medium screens
+        sm: '640px', // Small screens
+        md: '768px', // Medium screens
+        lg: '1024px', // Large screens
+        xl: '1280px', // Extra large screens
         '2xl': '1440px', // 2 Extra large screens
-        '3xl': '2000px'
+        '3xl': '2000px',
       },
       colors: {
-        customOrange: {
-          light: '#fdba74',    // Slightly lighter red
-          dark: '#fb923c',         // Slightly darker red
-        },
-        customBlack: 'rgb(24, 24, 56)',  // Define custom black color
-        customYellow: '#FACC15', // Define custom yellow color
+        customOrange:{
+          light: '#fdba74', // Slightly lighter orange
+          dark: '#fb923c', // Slightly darker orange
+        }
+        
       },
-      // note that images from public folder should be referenced from the root 
-      // of the proj
       backgroundImage: {
-        'parallax1': 'url("/pexels-palumalerba-2426546.jpg")',
-        'parallax2': 'url("/pexels-simo-3544415.jpg")'
-      },
-      // Custom font-variation-settings utility
-      fontVariationSettings: {
-        wdth: 100,  // Add custom font variations here
+        parallax1: 'url("/pexels-palumalerba-2426546.jpg")',
+        parallax2: 'url("/pexels-simo-3544415.jpg")',
       },
     },
   },
   plugins: [
-    flowbitePlugin,
     function ({ addUtilities }) {
       addUtilities({
         /* Hide scrollbar for Chrome, Safari, and Opera */
@@ -64,13 +47,12 @@ export default {
       });
     },
     function ({ addComponents }) {
-      // Add custom component for font with variation settings
       addComponents({
         '.open-sans-custom': {
           fontFamily: '"Open Sans", serif',
           fontStyle: 'normal',
-          fontWeight: '400',  // Adjust as needed (300, 600, etc.)
-          fontVariationSettings: '"wdth" 100',  // This applies the width variation
+          fontWeight: '400', // Adjust as needed (300, 600, etc.)
+          fontVariationSettings: '"wdth" 100', // This applies the width variation
         },
       });
     },
