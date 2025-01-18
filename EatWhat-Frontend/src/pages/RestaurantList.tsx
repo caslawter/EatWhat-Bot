4ab5ba6a151a22ddf5ab36629b3dc1acadc548d0
wei bin <lauweibin77@gmail.com>
2025-01-18 15:24:42 +0800

    restaurant list fixed

--- EatWhat-Frontend/src/pages/RestaurantList.tsx
+++ EatWhat-Frontend/src/pages/RestaurantList.tsx
@@ -48,62 +48,9 @@ const RestaurantCard = (): JSX.Element => {
         {/* Down */}
         <p>{restaurant_1.location}</p>
       </div>
-const RestaurantCard = (): JSX.Element => {
-  const [checked, setChecked] = useState<boolean>(false);
-  const restaurant_1: RestaurantObject = {
-    name: "muthu curry",
-    location: "82 Orchard Rd, Singapore 238839",
-    rating: 3.7,
-    priceLevel: 1,
-    type: "Indian",
-  };
-  return (
-    <>
-
-      <div className="bordrer-b-2 ">
-        {/* left and right */}
-        <div>
-          {/* top and down */}
-          <div className="flex gap-1 item-center">
-            {/* top */}
-            <h3 className="font-bold text-lg">{restaurant_1.name}</h3>
-            <p>{restaurant_1.type}</p>
-            <div className="flex items-center">
-              <FaDollarSign className="text-customOrange-dark" />
-              <FaDollarSign className="text-gray-300" />
-              <FaDollarSign className="text-gray-300" />
-              <FaDollarSign className="text-gray-300" />
-            </div>
-            <div className="flex items-center">
-              <FaStar className="text-customOrange-dark" />
-              <p>{restaurant_1.rating}</p>
-            </div>
-          </div>
-          {/* Down */}
-          <p>{restaurant_1.location}</p>
-        </div>
-
-      {/* right-checkbox here */}
-      <input
-        type="checkbox"
-        name="name"
-        value={restaurant_1.name}
-        className="w-4 h-4 text-orange-500 bg-gray-100 border-gray-300 rounded focus:ring-orange-500 dark:focus:ring-orange-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-      />
-    </div>
-  );
-};
-        {/* right-checkbox here */}
-        <input
-          type="checkbox"
-          name="name"
-          value={restaurant_1.name}
-          className="w-4 h-4 text-orange-500 bg-gray-100 border-gray-300 rounded focus:ring-orange-500 dark:focus:ring-orange-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-        />
       </div>
-    </>
-  );
-};
+      )
+}
 
 const RestaurantList = (): JSX.Element => {
   // const [restaurantDetail,setRestaurantList]=useState<null | RestaurantObject >(null);
@@ -158,12 +105,10 @@ const RestaurantList = (): JSX.Element => {
         </div>
       </div>
     </div>
-  );
-};
-
-export default RestaurantList;
     </>
   );
 };
+
+
 
 export default RestaurantList;
